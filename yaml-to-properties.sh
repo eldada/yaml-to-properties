a752434744a613dfd07a4af5baa83b791a573d1c
Eldad Assis <eldada@jfrog.com>
2021-02-01 09:02:28 +0200

    Use a single yq command for the conversion (from https://github.com/mikefarah/yq/discussions/699)

--- yaml-to-properties.sh
+++ yaml-to-properties.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 FILE=
-KEY=.
-LINE=
 
 errorExit () {
     echo; echo "ERROR: $1"; echo
@@ -12,17 +10,16 @@ errorExit () {
 usage () {
     cat << END_USAGE
 
-${SCRIPT_NAME} - Convert a section from a YAML formatted file to properties format
+${SCRIPT_NAME} - Convert a YAML formatted file to properties format
 
 Usage: ${SCRIPT_NAME} <options>
 
 -f | --file <name>                : [MANDATORY] Yaml file to process
--k | --key <name>                 : Key to process (default: .)
 -h | --help                       : Show this usage
 
 Examples:
 ========
-$ ${SCRIPT_NAME} --file example.yaml --key '.common.key1'
+$ ${SCRIPT_NAME} --file examples/simple.yaml
 
 END_USAGE
 
@@ -38,10 +35,6 @@ processOptions () {
         case "$1" in
             -f | --file)
                 FILE="$2"
-                shift 2
-            ;;
-            -k | --key)
-                KEY="$2"
                 shift 2
             ;;
             -h | --help)
@@ -60,46 +53,15 @@ checkFileExists () {
     [ -f "${FILE}" ] || errorExit "File ${FILE} does not exist"
 }
 
-checkKeyExists () {
-    if [[ ! ${KEY} =~ ^\. ]]; then KEY=".$KEY"; fi
-    [ "$(yq e "${KEY}" "${FILE}")" == "null" ] && errorExit "Key $KEY does not exist"
-}
-
 processYaml () {
-    local key=$1
-    local keys
-    local line
-    local value
-
-    line=$key
-
-    # Check if there are child keys
-    keys=$(yq e "$key | keys" "${FILE}" 2> /dev/null)
-    if [ $? -ne 0 ]; then
-        # Try to get a value if exists
-        value=$(yq e "$key" "${FILE}")
-        line=$(echo "$line=$value" | sed 's,^.,,g')
-        echo "$line"
-    else
-        # Get child keys and do a recursive call to function
-        keys=$(echo "$keys" | grep -v "^#" | sed 's,- ,,g' | tr '\n' ' ')
-
-        for k in ${keys}; do
-            if [ "$line" == "." ]; then
-                processYaml ".$k"
-            else
-                processYaml "$line.$k"
-            fi
-        done
-    fi
+    yq eval '.. | select((tag == "!!map" or tag == "!!seq") | not) | (path | join(".")) + "=" + .' "$FILE"
 }
 
 main () {
     checkYq
     processOptions "$@"
     checkFileExists
-    checkKeyExists
-    processYaml "$KEY"
+    processYaml
 }
 
 ######### Main #########
